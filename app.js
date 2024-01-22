d20d5a6d57013ff1705242ec4f526cd7d6f4843f
Robbie <Leon.Wh1te@proton.me>
2024-01-22 17:16:38 +0000

    with screen reader and random image

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const form = document.getElementById('form')
 const imageContainer = document.getElementById('img-container')
+
+const announcer = document.getElementById('announcer')
+let randomButton = document.getElementById(`randomize-button`)
+let images = []
+let currentImgIndex = 0
 form.addEventListener('submit' , function(event)
 {
     event.preventDefault()
@@ -13,9 +18,31 @@ form.addEventListener('submit' , function(event)
     let response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${queryParam}&client_id=AZ_4LsnWx6O5xRBgmv8rFnJUhMkfloyNuuyAVp3OBM8`)
     console.log(response)
     let data = await response.json()
-    console.log(data.results[0].urls.raw)
-
-    let img = document.createElement('img')
-    img.src = data.results[0].urls.raw
-    imageContainer.appendChild(img)
+    images = data.results
+    createImages(images[currentImgIndex])
+    // imageContainer.appendChild(img)
 }
+
+function createImages(image)
+{
+    imageContainer.innerHTML= ''
+    let imgTag= document.createElement('img')
+    imgTag.src = image.urls.raw
+    imgTag.alt = image.alt_description // sets alt tahg for screen readers
+    imageContainer.appendChild(imgTag)
+    announceAltText(image.alt_description)
+}
+
+function announceAltText(altText)
+{
+    announcer.textContent = `New image displayed: ${altText}`
+}
+
+randomButton.addEventListener(`click` ,  function()
+{
+ if(images.length > 0)
+ {
+    currentImgIndex= Math.floor(Math.random() * images.length)
+    createImages(images[currentImgIndex])  
+ }   
+})
